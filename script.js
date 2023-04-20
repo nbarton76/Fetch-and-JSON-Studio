cfbdf8e81511bd4b7bb148f6962046c0d7a40554
John Dwyer <19336208+Blushu@users.noreply.github.com>
2023-04-20 08:59:56 -0500

    Initial Commit

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // TODO: add code here
+
+window.addEventListener('load', function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(jsonResponse){
+            let container = document.getElementById("container");
+            let newMarkup = '';
+
+            for(let i = 0; i < jsonResponse.length; i++){
+                let skillsMarkup = jsonResponse[i].skills.join(', ');
+
+                let activeText = ' class="red"';
+                if(jsonResponse[i].active === true) {
+                    activeText = ' class="green"';
+                }
+
+                newMarkup += `<div class="astronaut">
+                    <div class="bio">
+                        <h3>${jsonResponse[i].firstName} ${jsonResponse[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${jsonResponse[i].hoursInSpace}</li>
+                            <li${activeText}>Active: ${jsonResponse[i].active}</li>
+                            <li>Skills: ${skillsMarkup}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${jsonResponse[i].picture}">
+                </div>`;
+            }
+
+            container.innerHTML = newMarkup;
+        });
+    });
+});
